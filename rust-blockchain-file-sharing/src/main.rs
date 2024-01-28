ed087c8a50319e298bf2e55e545f997b36b64fd0
mbn <66748817+CollinEdward@users.noreply.github.com>
2024-01-28 01:22:07 +0000

    initial code and crates

--- rust-blockchain-file-sharing/src/main.rs
+++ rust-blockchain-file-sharing/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Deserialize)]
+struct BlockConfig {
+  network: String,
 }
+
+fn read_blockchain_conf() -> Result<BlockConfig, Box<dyn std::error::Error>> {
+  let mut file = File::open("blockchain.conf")?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+  
+  let config: BlockConfig = serde_yaml::from_str(&contents)?;
+  
+  Ok(config)
+}
+
+fn main() {
+    match read_blockchain_conf() {
+        Ok(config) => println!("{:?}", config),
+        Err(e) => println!("Error: {:?}", e),
+    }
+}
+
